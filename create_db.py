dbec908eacecadc4d2d072f09cba0fa802ea59e9
Ericsson Xeon <seat25@lcc.com>
2023-07-02 13:27:00 +0800

    Try out

--- create_db.py
+++ create_db.py
@@ -3,12 +3,20 @@ import sqlite3
 def main():
     with sqlite3.connect("create_db.db") as db:
         db.execute('\
-                           CREATE TABLE if not exist \
+                           CREATE TABLE if not exists \
                            Student (\
                            id integer NOT NULL PRIMARY KEY AUTOINCREMENT,\
                            name text NOT NULL,\
                            age smallint NOT NULL)\
                            ')
+        
+        db.execute('''
+            INSERT INTO 
+            Student(name, age)
+            VALUES("Brian Rocksmith", 23)        
+        ''')
+
+        db.commit()
 
 if __name__ == "__main__":
     main()
