1d9d172e63b23045459ec39c003f13f34e08f435
EricXPalace <98503418+EricXPalace@users.noreply.github.com>
2023-08-13 12:01:07 +0000

    Day 16 ended.

---
+++ Day16/Demo03.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# 定義假資料
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y = [10, -1, 9, -2, 8, -3, 7, -4, 6, -5]
+
+# 定義顏色等等美術細節
+
+
+# 繪製階層圖
+plt.plot(x, y, label="A Label", marker="o")
+
+# 定義標籤
+plt.xlabel("X-line")
+plt.ylabel("Y-line")
+plt.title("Title")
+
+
+# 顯示圖表
+plt.legend(loc = "upper right")
+plt.show()
--- main.py
+++ main.py
@@ -1,23 +1,43 @@
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
-import numpy as np
+from data import sales_data, annual_sales
 
-# 定義假資料
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y = [10, -1, 9, -2, 8, -3, 7, -4, 6, -5]
+fig1,ax1 = plt.subplots()
+ax1.bar(sales_data.keys(), sales_data.values())
+ax1.set_title("Sales Data")
+ax1.set_xlabel("Product")
+ax1.set_ylabel("Sales")
 
-# 定義顏色等等美術細節
+fig2,ax2 = plt.subplots()
+ax2.bar(annual_sales.keys(), annual_sales.values())
+ax2.set_title("Annual Sales")
+ax2.set_xlabel("Salesman")
+ax2.set_ylabel("Sales")
 
+root = tk.Tk()
 
-# 繪製柱狀圖
-plt.boxplot(x,label=["Python"],patch_artist=True)
-plt.boxplot(y)
+root.title("matplotlib")
+root.state("normal")
 
-# 定義標籤
-plt.xlabel("X-line")
-plt.ylabel("Y-line")
-plt.title("Title")
+side_frame = tk.Frame(root, bg="#198964")
+side_frame.pack(side="left", fill="y")
 
+label = tk.Label(side_frame, text="Dashboard", bg="#aa2525", fg="#FFFFFF", font=25)
+label.pack(pady=50,padx=20)
 
-# 顯示圖表
-plt.legend(loc = "upper right")
-plt.show()
+charts_frame = tk.Frame(root)
+charts_frame.pack()
+
+upper_fragme = tk.Frame(charts_frame)
+upper_fragme.pack(fill="both", expand=True)
+
+canvas1 = FigureCanvasTkAgg(fig1, upper_fragme)
+canvas1.draw()
+canvas1.get_tk_widget().pack(side="left", fill="both", expand=True)
+
+canvas2 = FigureCanvasTkAgg(fig1, upper_fragme)
+canvas2.draw()
+canvas2.get_tk_widget().pack(side="left", fill="both", expand=True)
+
+root.mainloop()
