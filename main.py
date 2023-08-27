c103a4d1a4438e9559150aac2f2a99f966a196b9
Ericsson <llc205.seat25@llc.com>
2023-08-27 15:57:28 +0800

    minor modify on main.py

--- main.py
+++ main.py
@@ -8,20 +8,15 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 
 with webdriver.Chrome(options=chrome_options) as driver:
   driver.maximize_window()
-  
+  # 取得聯成電腦官網的連結
   driver.get("https://member.lccnet.com.tw/")
-  print("[LOG] driver: get SUCCESS")
-  search_frame = driver.find_element(By.ID ,'Account')
-  print("[LOG] search_frame: find_element 'Account' SUCCESS")
-  search_frame.send_keys("105684713")
-  print("[LOG] search_frame: send_keys SUCCESS")
-  search_frame = driver.find_element(By.ID ,'Password')
-  print("[LOG] search_frame: find_element 'Password' SUCCESS")
-  search_frame.send_keys("A128450607")
-  print("[LOG] search_frame: send_keys SUCCESS")
-  search_button = driver.find_element(By.CLASS_NAME ,'loginBtn')
-  print("[LOG] search_button: find_element SUCCESS")
-  search_button.click()
-  print("[LOG] search_button: click SUCCESS")
+  print("[LOG] driver: get succeed.")
+  # 輸入我自己的帳戶密碼登入
+  driver.find_element(By.ID ,'Account').send_keys("105684713")
+  driver.find_element(By.ID ,'Password').send_keys("A128450607")
+  driver.find_element(By.CLASS_NAME ,'loginBtn').click()
+  print("[LOG] Login prograss succeed.")
+  # 找到我要工作點擊
+  driver.find_element(By.TAG_NAME, "我要就業").click()
 
-input("如果看到此訊息，表示一切執行正常，請按任意鍵終止程式。")
+  input("如果看到此訊息，表示一切執行正常，請按任意鍵終止程式。")
